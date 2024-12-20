7901a07ed7aca2ecc73e1926c3de3888f9de2ccc
KuromiVoid1007 <coolvip1007@mail.ru>
2024-12-21 02:49:08 +0500

    swiper
    limk swiper
    +js

---
+++ script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,        // Показывать 1 слайд
+    spaceBetween: 10,        // Отступ между слайдами
+    loop: true,              // Бесконечная прокрутка
+    grabCursor: true,        // Курсор-рука при наведении на слайдер
+    simulateTouch: true,     // Поддержка перетаскивания слайдов мышью
+    touchEventsTarget: 'container', // На контейнере происходит перетаскивание
+    autoplay: {
+      delay: 5000,           // Задержка между переключениями (в миллисекундах)
+    },
+    
+    // Отключаем элементы навигации и пагинацию
+    navigation: false,
+    pagination: false,
+  });
